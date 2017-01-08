8debeb427b2a915b79595fc2d518d9d45117c8d7
Suzumiya <jrgwyz@163.com>
2017-01-08 22:34:51 +0800

    Routie add

--- routine.cpp
+++ routine.cpp
@@ -1,5 +1,15 @@
 class IntSetStl {
 private:
     set<int> S;
-
+public:
+    IntSetStl(int maxelms, int maxval) { }
+    int size() { return S.size(); }
+    void insert(int t) { S.insert(t); }
+    void report(int *v)
+    {   int j = 0;
+        set<int>::iterator i;
+        for (i = S.begin(); i != S.end(); ++i) {
+            v[j++] = *i;
+        }
+    }
 };
