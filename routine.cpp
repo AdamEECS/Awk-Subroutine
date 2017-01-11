1b9db784b3c38769c1448f72ebb99967d76dda34
Suzumiya <jrgwyz@163.com>
2017-01-11 22:54:44 +0800

    Routine add

--- routine.cpp
+++ routine.cpp
@@ -23,4 +23,19 @@ public:
         n = 0;
         x[0] = maxval;
     }
+    int size() { return n; }
+    void insert(int t)
+    {   for(int i = 0; x[i] < t; i++)
+            ;
+        if (x[i] == t)
+            return;
+        for (int j = n; j >= i; j--)
+            x[j+1] = x[j];
+        x[i] = t;
+        n++;
+    }
+    void report(int *v)
+    {   for (int i = 0; i < n; i++)
+            v[i] = x[i];
+    }
 };
